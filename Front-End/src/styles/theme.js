f7cb60a83473f6e8621698b253b7adf93d71df26
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-16 19:54:03 -0300

    style: Configure colors for project

---
+++ Front-End/src/styles/theme.js
@@ -0,0 +1,23 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#1C1B1E',
+    BACKGROUND_800: '#262529',
+    BACKGROUND_700: '#282124',
+    BACKGROUND_600: '#312E38',
+    BACKGROUND_500: '#3E3B47',
+    BACKGROUND_SHADOW: '#1C1B1Eb3',
+
+    WHITE_500: '#FFFFFF',
+    WHITE_400: '#F4EDE8',
+    
+    GRAY_100: '#E5E5E5',
+    GRAY_300: '#CAC4CF',
+    GRAY_500: '#948F99',
+    
+    BLACK_500: '#000000',
+    BLACK_400: '#0D0C0F',
+
+    PINK_500: '#FF859B',
+    PINK_SHADOW: '#FF859Bd',
+  }
+}
