dab8791826f2734a0f817db59b0bacbf836f4946
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-17 18:55:38 -0300

    feat: Create and configure Button component

---
+++ Front-End/src/components/Button/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+  padding-block: 1.68rem;
+  border: none;
+  border-radius: 1rem;
+
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  font-weight: 500;
+  font-size: 1.6rem;
+  line-height: 2.1rem;
+
+  background: ${({ theme }) => theme.COLORS.PINK_500};
+`
