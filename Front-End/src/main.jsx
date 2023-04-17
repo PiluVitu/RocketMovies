8b34c4978c555d1060ca7f4de481cd5a127ce315
Paulo Victor Torres Silva <paulo.tspi@gmail.com>
2023-04-17 19:03:22 -0300

    feat: insert routes on main, to work correctely on app

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
-import { SignIn } from './pages/SignIn'
+
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
